b7a3de1f7c3007c1fc67bf57de0b910a09077669
Grigori Kochanov <grigori@grik.net>
2020-12-06 07:43:49 +0200

    Copying a simple parent class to the package

---
+++ packages/acme-image/src/lib/BaseModel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Acme\Image\lib;
+
+/**
+ * A copy of a simple base class to show inheritance decomposition
+ * @internal
+ * @package Acme/Image
+ */
+class BaseModel
+{
+    /**
+     *
+     */
+    protected function foo()
+    {
+    }
+
+    static function bar()
+    {
+    }
+}
